a97e6bcea8dd65ba780999e508decbfd83227a65
Nathieee <nathbivan@gmail.com>
2023-11-08 00:40:31 +0100

    new app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+// import ScrollToTop from "./utils/ScrollToTop";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLogin from "./pages/auth/login/UserLogin";
 import UserRegister from "./pages/auth/register/UserRegister";
@@ -7,35 +8,23 @@ import NewPaswdPage from "./pages/NewPaswdPage";
 import ResetPaswdPage from "./pages/ResetPaswdPage";
 import HomePage from "./pages/user/HomePage"
 import AddressPage from "./pages/user/AddressPage";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import HowItWorks from "./pages/HowItWorks";
-import DatePage from "./pages/DatePage";
-import UserProfilePage from "./pages/UserProfilePage";
-import UserDashboardContactPage from "./pages/UserDashboardContactPage";
-import UserOrderDetailsPage from "./pages/UserOrderDetailsPage";
- 
-=======
-=======
-import PaymentPage from "./pages/PaymentPage";
-import ClothesSelection from "./pages/ClothesSelection";
->>>>>>> 2313b30119392b7fcf56582132523b1ffc3ecd09
 import PricingPage from "./pages/PricingPage";
 import Services from "./pages/Services";
 import About from "./pages/About";
 import MarketPlace from "./pages/MarketPlace/MarketPlace";
 import Orders from "./pages/user/Orders";
 import OrderReceipt from "./pages/user/OrderReceipt";
+
 import SingleProduct from "./pages/MarketPlace/SingleProduct";
 import Wishlist from "./pages/MarketPlace/Wishlist"
 import Cart from "./pages/MarketPlace/Cart" 
 
->>>>>>> c67370096f3b7254975a7d1d8b618f7a59de02ed
 
 
 function App() {
   return (
     <BrowserRouter>
+      {/* <ScrollToTop /> */}
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/address" element={<AddressPage/>} />
@@ -44,21 +33,6 @@ function App() {
         <Route path="/auth/reset-password" element={<ResetPaswdPage />} />
          <Route path="/auth/new-password" element={<NewPaswdPage />} />
          <Route path="/auth/verify" element={<AuthenticatePaswdPage />} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-         <Route path="/how-it-works" element={<HowItWorks />} />
-         <Route path="/date" element={<DatePage />} />
-         <Route path="/user-profile" element={<UserProfilePage />} />
-         <Route path="/dashboard-contact-us" element={<UserDashboardContactPage />} />
-         <Route path="/order-details" element={<UserOrderDetailsPage />} />
-        
-      </Routes>
-    </BrowserRouter>
-=======
-=======
-        <Route path="/ClothesSelection" element={<ClothesSelection/>}/>
-        <Route path="/PaymentPage" element={<PaymentPage/>}/>
->>>>>>> 2313b30119392b7fcf56582132523b1ffc3ecd09
          <Route path="/pricing" element={<PricingPage />} />
          <Route path="/ourservices" element={<Services/>} />
          <Route path="/about" element={<About/>} />
@@ -66,13 +40,16 @@ function App() {
          <Route path="/singleproduct" element={<SingleProduct/>} />
          <Route path="/wishlist" element={<Wishlist/>} />
          <Route path="/cart" element={<Cart/>} />
+
         <Route path="/my-orders" element={<Orders/>}/>
         <Route path="/order-receipt" element={<OrderReceipt/>}/>
-        
+        <Route path="/date" element={<DatePage />} />
+         <Route path="/user-profile" element={<UserProfilePage />} />
+         <Route path="/dashboard-contact-us" element={<UserDashboardContactPage />} />
+         <Route path="/order-details" element={<UserOrderDetailsPage />} />
       </Routes>
     </BrowserRouter>
 
->>>>>>> c67370096f3b7254975a7d1d8b618f7a59de02ed
   );
 }
 
