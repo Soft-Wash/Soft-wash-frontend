f30766dd5ea5145b3a5063b6016112e7aaf2e9b4
emixwrld <emixwrld@gmail.com>
2023-11-11 12:12:20 +0100

    copied updated IncandDec button

--- src/components/ClothesSelectionPage/ClothesSelectCart.js
+++ src/components/ClothesSelectionPage/ClothesSelectCart.js
@@ -38,7 +38,25 @@ function SelectedCart(){
                                                     <h5>Native</h5>
                                                     <span>2 x N2,000 / per piece</span>
                                                 </div>
-                                                <ClothesSelectCounter/>
+                                                <div className="IncDecButton">
+                                                    <div className="d-flex text alaign " style={{height:"35px", marginLeft:"450px"}} >
+                                                    <button className='-ve' onClick={decrement}>-</button>
+                                                    <input
+                                                        type="text"
+                                                        step="1"
+                                                        min="0"
+                                                        max=""
+                                                        name="quantity"
+                                                        value={quantity}
+                                                        title="Qty"
+                                                        className="input-text qty text "
+                                                        size="4"
+                                                        pattern=""
+                                                        inputMode=""
+                                                        />
+                                                        <button className='pve' onClick={increment}>+</button>
+                                                    </div>
+                                                </div>
                                             </div>
                                             </div>
                                             
--- src/components/ClothesSelectionPage/ClothesSelectCounter.js
+++ src/components/ClothesSelectionPage/ClothesSelectCounter.js
@@ -1,20 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import ButtonIncDec from './ButtonIncDec';
-function ClothesSelectCounter() {
-    const initialQuantities = [0]; // Initial quantities for different items
+import React, { useState } from 'react';
+// import ReactDOM from 'react-dom';
+
+function ClothesSelectCounter({initialQuantity}) {
+  
+
+    const [quantity, setQuantity] = useState(initialQuantity);
+
+    const increment = () => {
+setQuantity((prevquantity=>prevquantity+1))
+    };
+  
+    const decrement = () => {
+      setQuantity(prevquantity=>Math.mix(prevquantity-1,0))
+    };
 
   return (
     <div>
-      {initialQuantities.map((quantity, index) => (
-        <div key={index}>
-            <ButtonIncDec initialQuantity={quantity} />
-        </div>
-      ))}
+      <div className="d-flex text alaign " style={{height:"35px", marginLeft:"450px"}} >
+        <button className='-ve' onClick={decrement}>-</button>
+        <input
+  type="text"
+  step="1"
+  min="0"
+  name="quantity"
+  value={quantity}
+  title="Qty"
+  className="input-text qty text"
+  size="4"
+  pattern=""
+  inputMode=""
+/>
+
+        <button className='pve' onClick={increment}>+</button>
+      </div>
     </div>
   );
   
 }
 
-ReactDOM.render(<ClothesSelectCounter />, document.getElementById('root'));
 export default ClothesSelectCounter
