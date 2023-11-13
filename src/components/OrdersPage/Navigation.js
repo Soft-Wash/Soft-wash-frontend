6de90a3a67208d1986c24c522e09a0623782f10f
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-11-13 16:37:58 +0100

    testimonial was not showing up

--- src/components/OrdersPage/Navigation.js
+++ src/components/OrdersPage/Navigation.js
@@ -5,18 +5,18 @@ import img from "../../assets/icons/linkedin.png";
 function Navigation() {
   return (
     <div className="user-dashboard-nav  bg-body-white shadow-sm pe-3" style={{height:"55px"}}>
-      <div className="my-auto position-relative fs-6" style={{ top: "0px", right: "0px", height: "20px", width: "40px" }}>
+      <div className=" position-relative fs-6" style={{ top: "0px", right: "0px", height: "20px", width: "40px" }}>
         <small
           className=" d-flex align-items-center p-1 position-absolute bg-danger text-white fs-6 rounded-circle border border-white"
           style={{top:"-4px", right:"9px", height:"20px"}}
         >
           3
         </small>
-
         <BsBell className=" fs-4 mr-0" />
       </div>
-      <div className="my-auto">
-        <Dropdown className="d-inline mx-2 my-auto"
+      
+      <div className="my-auto h-100">
+        <Dropdown className="d-inline mx-2 my-auto h-100"
         align={{ lg: 'end', sm: "start"}}>
             
           <Dropdown.Toggle
