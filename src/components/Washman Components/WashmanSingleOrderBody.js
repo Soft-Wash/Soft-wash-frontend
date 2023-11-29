ccc50e79bc832506796c6b6c4f096e5aa93dd0eb
Gbadass <theboyfakaa@gmail.com>
2023-11-29 07:59:37 +0100

    updated order tracking

--- src/components/Washman Components/WashmanSingleOrderBody.js
+++ src/components/Washman Components/WashmanSingleOrderBody.js
@@ -11,137 +11,137 @@ import ProgressBar from "react-bootstrap/ProgressBar";
 import { FaCheck } from "react-icons/fa";
 
 function WashmanSingleOrderBody() {
-	const [progress, setProgress] = useState(0);
-	const [selectedOption, setSelectedOption] = useState("Order Placed");
-	const [order, setOrder] = useState(null);
-	const { _id } = useParams();
+  const [progress, setProgress] = useState(0);
+  const [selectedOption, setSelectedOption] = useState("Order Placed");
+  const [order, setOrder] = useState(null);
+  const { _id } = useParams();
 
-	useEffect(() => {
-		axios
-			.get(`${process.env.REACT_APP_BASE_URL}/order/${_id}/order`)
-			.then((resp) => {
-				console.log(resp);
-				setOrder(resp.data);
-			})
-			.catch((error) => {
-				console.error("Error fetching order:", error);
-			});
-	}, []);
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/order/${_id}/order`)
+      .then((resp) => {
+        console.log(resp);
+        setOrder(resp.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching order:", error);
+      });
+  }, []);
 
-	const handleButtonClick = () => {
-		setProgress((prevProgress) => Math.min(prevProgress + 14.29, 100));
-	};
+  const handleButtonClick = () => {
+    setProgress((prevProgress) => Math.min(prevProgress + 14.29, 100));
+  };
 
-	const handleSeleectChange = async (e) => {
-		setSelectedOption(e.target.value);
+  const handleSeleectChange = async (e) => {
+    setSelectedOption(e.target.value);
 
-		if (selectedOption === "Order Placed") {
-			console.log(selectedOption);
-			axios
-				.put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
-					status: selectedOption,
-				})
-				.then((resp) => {
-					console.log(resp);
-				});
-		} else if (selectedOption === "Confirmed") {
-			console.log(selectedOption);
-			axios
-				.put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
-					status: selectedOption,
-				})
-				.then((resp) => {
-					console.log(resp);
-				});
-		} else if (selectedOption === "Recieved") {
-			console.log(selectedOption);
-			axios
-				.put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
-					status: selectedOption,
-				})
-				.then((resp) => {
-					console.log(resp);
-				});
-		} else if (selectedOption === "Cleaning") {
-			console.log(selectedOption);
-			axios
-				.put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
-					status: selectedOption,
-				})
-				.then((resp) => {
-					console.log(resp);
-				});
-		} else if (selectedOption === "Ready") {
-			console.log(selectedOption);
-			axios
-				.put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
-					status: selectedOption,
-				})
-				.then((resp) => {
-					console.log(resp);
-				});
-		} else if (selectedOption === "Shipped") {
-			console.log(selectedOption);
-			axios
-				.put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
-					status: selectedOption,
-				})
-				.then((resp) => {
-					console.log(resp);
-				});
-		} else if (selectedOption === "Delivered") {
-			console.log(selectedOption);
-			axios
-				.put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
-					status: selectedOption,
-				})
-				.then((resp) => {
-					console.log(resp);
-				});
-		}
-	};
+    if (selectedOption === "Order Placed") {
+      console.log(selectedOption);
+      axios
+        .put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
+          status: selectedOption,
+        })
+        .then((resp) => {
+          console.log(resp);
+        });
+    } else if (selectedOption === "Confirmed") {
+      console.log(selectedOption);
+      axios
+        .put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
+          status: selectedOption,
+        })
+        .then((resp) => {
+          console.log(resp);
+        });
+    } else if (selectedOption === "Recieved") {
+      console.log(selectedOption);
+      axios
+        .put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
+          status: selectedOption,
+        })
+        .then((resp) => {
+          console.log(resp);
+        });
+    } else if (selectedOption === "Cleaning") {
+      console.log(selectedOption);
+      axios
+        .put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
+          status: selectedOption,
+        })
+        .then((resp) => {
+          console.log(resp);
+        });
+    } else if (selectedOption === "Ready") {
+      console.log(selectedOption);
+      axios
+        .put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
+          status: selectedOption,
+        })
+        .then((resp) => {
+          console.log(resp);
+        });
+    } else if (selectedOption === "Shipped") {
+      console.log(selectedOption);
+      axios
+        .put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
+          status: selectedOption,
+        })
+        .then((resp) => {
+          console.log(resp);
+        });
+    } else if (selectedOption === "Delivered") {
+      console.log(selectedOption);
+      axios
+        .put(`${process.env.REACT_APP_BASE_URL}/order/${_id}/update`, {
+          status: selectedOption,
+        })
+        .then((resp) => {
+          console.log(resp);
+        });
+    }
+  };
 
-	return (
-		<div className="washman-bg">
-			<div className="washman-page-content">
-				<div className="washman-header">
-					<h2>ORDER DETAILS</h2>
-				</div>
-				<div className="washman-single-order-content">
-					{order &&
-						Object.entries(order).map(([key, value]) =>
-							typeof value === "string" ? (
-								<div key={key} className="washman-profile-field">
-									<h4>{key}</h4>
-									<h4>{value}</h4>
-								</div>
-							) : null
-						)}
+  return (
+    <div className="washman-bg">
+      <div className="washman-page-content">
+        <div className="washman-header">
+          <h2>ORDER DETAILS</h2>
+        </div>
+        <div className="washman-single-order-content">
+          {order &&
+            Object.entries(order).map(([key, value]) =>
+              typeof value === "string" ? (
+                <div key={key} className="washman-profile-field">
+                  <h4>{key}</h4>
+                  <h4>{value}</h4>
+                </div>
+              ) : null
+            )}
 
-					<div className="washman-profile-field">
-						<h4>Order Status</h4>
-						<select onChange={(e) => setSelectedOption(e.target.value)}>
-							<option value="Order Placed">ORDER PLACED</option>
-							<option value="Confirmed">CONFIRMED</option>
-							<option value="Recieved">RECEIVED</option>
-							<option value="Cleaning">CLEANING</option>
-							<option value="Ready">READY</option>
-							<option value="Shipped">SHIPPED</option>
-							<option value="Delivered">DELIVERED</option>
-						</select>
-					</div>
-				</div>
-				<Button
-					className="edit-washman-profile-btn"
-					onClick={handleSeleectChange}
-				>
-					Update Order
-				</Button>
-				<div className="progressBar">
-					<Col lg={12} md={6} sm={10} className="text-center position-relative">
+          <div className="washman-profile-field">
+            <h4>Order Status</h4>
+            <select onChange={(e) => setSelectedOption(e.target.value)}>
+              <option value="Order Placed">ORDER PLACED</option>
+              <option value="Confirmed">CONFIRMED</option>
+              <option value="Recieved">RECEIVED</option>
+              <option value="Cleaning">CLEANING</option>
+              <option value="Ready">READY</option>
+              <option value="Shipped">SHIPPED</option>
+              <option value="Delivered">DELIVERED</option>
+            </select>
+          </div>
+        </div>
+        <Button
+          className="edit-washman-profile-btn"
+          onClick={handleSeleectChange}
+        >
+          Update Order
+        </Button>
+        {/* <div className="progressBar  position-relative">
+					<Col lg={12} md={6} sm={10} className="text-center">
 						<Row classname="w-100 align-centre">
-							<Container className=" d-flex gap-5 mt-4 position-relative">
-								<div className="order-progress-circle position-absolute ">
+							<Container className="containerOfCircles d-flex gap-5 mt-4 position-absolute">
+								<div className="order-progress-circle">
 									<Col
 										className={`duration-500 border border-4  ${
 											progress >= 14.29 ? "border-info" : null
@@ -157,7 +157,7 @@ function WashmanSingleOrderBody() {
 									<h6>ORDER PLACED</h6>
 								</div>
 
-								<div className="order-progress-circle">
+								<div className="order-progress-circle2">
 									<Col
 										className={`duration-500 border border-4 ${
 											progress > 28.58 ? "border-info" : null
@@ -173,7 +173,7 @@ function WashmanSingleOrderBody() {
 									<h6>CONFIRMED</h6>
 								</div>
 
-								<div className="order-progress-circle">
+								<div className="order-progress-circle3">
 									<Col
 										className={`duration-500 border border-4 ${
 											progress > 42.87 ? "border-info" : null
@@ -255,8 +255,8 @@ function WashmanSingleOrderBody() {
 							</Container>
 
 							<Col
-								className="px-4 position-relative"
-								style={{ top: "0", zIndex: "1" }}
+								className="px-4 position-absolute"
+								style={{ top: "50px", zIndex: "3" }}
 							>
 								<ProgressBar
 									now={progress}
@@ -267,13 +267,33 @@ function WashmanSingleOrderBody() {
 							</Col>
 						</Row>
 					</Col>
-				</div>
-			</div>
-                <button className="btn" onClick={() => handleButtonClick()}>
-                Next
-                </button>
-		</div>
-	);
+				</div> */}
+        <div className="progress-status-div">
+				<Col className="progress-bar-col">
+            <ProgressBar now={progress} variant="info" className="progress-bar" />
+          </Col>
+          <Container className="progress-status-div-container">
+            <Row className="progress-status-div-row">
+              <div className={`progress-status-div-cirle1 ${progress >= 14.29 ? "border-info" : null}`}>
+							<FaCheck className="progress-bar-ckeck" />
+							</div>
+							<div className={`progress-status-div-cirle2 ${progress >= 28.58 ? "border-info" : null}`}></div>
+              <div className={`progress-status-div-cirle3 ${progress >= 42.87 ? "border-info" : null}`}></div>
+							<div className={`progress-status-div-cirle4 ${progress >= 57.16 ? "border-info" : null}`}></div>
+              <div className={`progress-status-div-cirle5 ${progress >= 71.54 ? "border-info" : null}`}></div>
+							<div className={`progress-status-div-cirle6 ${progress >= 85.74 ? "border-info" : null}`}></div>
+              <div className={`progress-status-div-cirle7 ${progress >= 100.03 ? "border-info" : null}`}></div>
+
+            </Row>
+          </Container>
+
+        </div>
+      </div>
+      <button className="btn" onClick={() => handleButtonClick()}>
+        Next
+      </button>
+    </div>
+  );
 }
 
 export default WashmanSingleOrderBody;
