b2aeaec8bcbd7700e8182be9f8eefc205901e813
Gbadass <theboyfakaa@gmail.com>
2023-12-03 21:17:41 +0100

    updated order status

--- src/pages/Admin/OrderTable.js
+++ src/pages/Admin/OrderTable.js
@@ -3,30 +3,28 @@ import AdminSidebar from "../../components/Admin/AdminSidebar";
 import "../../styles/Admin/tableorder.css";
 import { axiosInstance } from "../../services/AxiosInstance";
 import { useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function OderTable() {
   const [orders, setOrders] = useState();
   const [selectedOption, setSelectedOption] = useState("All Orders");
-
-
-
+  const [statusSelect, setStatusSelect] = useState();
+  const [statusData, setStatusData] = useState();
 
   const fetchData = () => {
     if (selectedOption === "All Orders") {
       axiosInstance.get("/order").then((resp) => {
         setOrders(resp.data);
       });
-    } else if(selectedOption==="Daily Orders"){
+    } else if (selectedOption === "Daily Orders") {
       axiosInstance.get("/order/day").then((resp) => {
         setOrders(resp.data);
       });
-    } else if(selectedOption==="Weekly Orders"){
+    } else if (selectedOption === "Weekly Orders") {
       axiosInstance.get("/order/week").then((resp) => {
         setOrders(resp.data);
-        
       });
-    } else if(selectedOption==="Monthly Orders"){
+    } else if (selectedOption === "Monthly Orders") {
       axiosInstance.get("/order/month").then((resp) => {
         setOrders(resp.data);
       });
@@ -34,33 +32,45 @@ function OderTable() {
   };
 
   const handleSelectChange = (e) => {
-    setSelectedOption(e.target.value);
+    if (e.target.name.startsWith("statusOrder")) {
+      setStatusSelect(e.target.value);
+    } else {
+      setSelectedOption(e.target.value);
+    }
   };
 
   useEffect(() => {
     fetchData();
-  }, [selectedOption]);
+    getOrderStatus();
+  }, [selectedOption, statusSelect]);
 
+  const getOrderStatus = () => {
+    axiosInstance.get(`/order/status?status=${statusSelect}`).then((resp) => {
+      console.log(statusSelect);
+      console.log(resp.data);
+      setStatusData(resp.data);
+    });
+  };
 
   const getStatusColorClass = (status) => {
     switch (status) {
-      case 'order placed':
-        return 'order-placed';
-      case 'Received':
-        return 'received-color';
-      case 'Cleaning':
-        return 'Cleaning-color';
-      case 'Confirmed':
-          return 'confirmed-color';  
-      case 'Shipped':
-            return 'shipped-color'; 
-      case 'Ready':
-            return 'ready-color'
-      case 'Delivered':
-            return 'delivered-color'          
+      case "order placed":
+        return "order-placed";
+      case "Received":
+        return "received-color";
+      case "Cleaning":
+        return "Cleaning-color";
+      case "Confirmed":
+        return "confirmed-color";
+      case "Shipped":
+        return "shipped-color";
+      case "Ready":
+        return "ready-color";
+      case "Delivered":
+        return "delivered-color";
 
       default:
-        return ''; 
+        return "";
     }
   };
 
@@ -70,14 +80,22 @@ function OderTable() {
         <AdminSidebar />
         <div className="ordertable-div">
           <div>
-            <select name="" className="select-dropdown" id="" onChange={handleSelectChange}>
-            <option value="All Orders">All Order</option>
-              <option value="Daily Orders">
-                Daily Order
+            <select
+              name="statusOrder"
+              className="select-dropdown2"
+              id=""
+              onChange={handleSelectChange}
+            >
+              <option value="" hidden>
+                Select Order Status
               </option>
-              <option value="Weekly Orders">Weekly Order</option>
-              <option value="Monthly Orders">Monthly Order</option>
-              <option value="Yearly Orders">Yearly Order</option>
+              <option value="order placed">ORDER PLACED</option>
+              <option value="Confirmed">CONFIRMED</option>
+              <option value="Received">RECEIVED</option>
+              <option value="Cleaning">CLEANING</option>
+              <option value="Ready">READY</option>
+              <option value="Shipped">SHIPPED</option>
+              <option value="Delivered">DELIVERED</option>
             </select>
           </div>
           <table className="ordertbale-content-table">
@@ -93,8 +111,15 @@ function OderTable() {
               </tr>
             </thead>
             <tbody>
-              {orders &&
-                orders.map((item) => (
+              {statusData.length<1 ? (
+                <tr>
+                  <td colSpan="6" className="no-data-message">
+                    No data available
+                  </td>
+                </tr>
+              ) : (
+                statusData &&
+                statusData.map((item) => (
                   <tr key={item._id}>
                     <th>{item._id.substring(0, item._id.length / 2)}</th>
                     <th>{item?.branch_id?.name}</th>
@@ -102,19 +127,27 @@ function OderTable() {
                     <th>{item?.deliveryAddress[0]?.FullAddress}</th>
                     <th>{item?.subtotal}</th>
                     <th className="allorders-status-th">
-                      <button className={`status-button ${getStatusColorClass(item?.status)}`}> {item?.status}</button>
-                      </th>
-                      <th>
+                      <button
+                        className={`status-button ${getStatusColorClass(
+                          item?.status
+                        )}`}
+                      >
+                        {" "}
+                        {item?.status}
+                      </button>
+                    </th>
+                    <th>
                       <div className="d-flex">
                         <Link to={`/adminsingleorder/${item._id}`}>
-                        <button className="action-buttons-btn1">View</button>
+                          <button className="action-buttons-btn1">View</button>
                         </Link>
 
                         <button className="action-buttons-btn2">Print</button>
-                        </div>
-                      </th>
+                      </div>
+                    </th>
                   </tr>
-                ))}
+                ))
+              )}
             </tbody>
           </table>
         </div>
