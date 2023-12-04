fb74c674a49ebcd1aa2a6209cef1fc301bc4ee7c
emixwrld <emixwrld@gmail.com>
2023-12-04 22:01:58 +0100

    Created mployeeLogin services

---
+++ src/services/EmployeeLogin.js
@@ -0,0 +1,15 @@
+import { axiosInstance } from "./AxiosInstance";
+
+export async function handleLogin(payload){
+  let data,error;
+  try {
+    const response = await axiosInstance.post(`/auth/employee/login`, payload);
+    data = response.data.noPasswordUser;
+    localStorage.setItem("softwashLoginUser", JSON.stringify(data));
+
+  } catch (err) {
+    error = err;
+  }
+  return {data,error};
+
+}
