88f2b9186cef05ce0f02f9722194411af11dc754
Gbadass <theboyfakaa@gmail.com>
2023-12-23 10:58:25 +0100

    updated createorder

--- src/pages/Admin/CreatOrder.js
+++ src/pages/Admin/CreatOrder.js
@@ -1,32 +1,47 @@
 import AdminSidebar from "../../components/Admin/AdminSidebar";
-import "../../styles/Admin/CreateOrder.css"
+import "../../styles/Admin/CreateOrder.css";
+import { useEffect, useState } from "react";
+import { axiosInstance } from "../../services/AxiosInstance";
+import Modal from "react-bootstrap/Modal";
 
-function CreateOrder(){
-  return(
+function CreateOrder() {
+  const [clothTypes, setclothTypes] = useState();
+
+  useEffect(() => {
+    axiosInstance.get("cloth/").then((resp) => {
+      console.log(resp.data);
+      setclothTypes(resp.data);
+    });
+  }, []);
+
+  return (
     <div>
-<div className="d-flex">
-  <AdminSidebar/>
-  <div className="createorder-content-container">
-    <div className="createorder-content">
-    <h3>Add Order</h3>
-    <hr  className="createorder-content-hr"/>
-    <div className="cloth-card">
-    <div className="cloth-card-div1">
-<div className="cloth-border">
+      <div className="d-flex">
+        <AdminSidebar />
+        <div className="createorder-content-container">
+          <div className="createorder-content">
+            <h3>Add Order</h3>
+            <hr className="createorder-content-hr" />
+            <div className="cloth-card">
+              <div className="cloth-card-div1">
+                {clothTypes &&
+                  clothTypes.map((item) => (
+                    <>
+                    <div className="cloth-border">
+                      <img src={item?.img} alt="" />
+                      <p className="cloth-border-p">{item.name}</p>
+                    </div>
 
-</div>
+                    </>
+                  ))}
+              </div>
+              <div className="cloth-card-div2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="cloth-card-div2">
-
-</div>
-    </div>
-    </div>
-
-
-  </div>
-</div>
-    </div>
-  )
+  );
 }
 
 export default CreateOrder;
