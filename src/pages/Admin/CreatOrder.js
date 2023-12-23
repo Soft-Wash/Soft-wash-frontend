a7568373f9b97080fb206560a9fc7c0a7076550f
Gbadass <theboyfakaa@gmail.com>
2023-12-23 09:39:39 +0100

    updated created order

--- src/pages/Admin/CreatOrder.js
+++ src/pages/Admin/CreatOrder.js
@@ -6,7 +6,22 @@ function CreateOrder(){
     <div>
 <div className="d-flex">
   <AdminSidebar/>
-  <div className="CreateOrder-content">
+  <div className="createorder-content-container">
+    <div className="createorder-content">
+    <h3>Add Order</h3>
+    <hr  className="createorder-content-hr"/>
+    <div className="cloth-card">
+    <div className="cloth-card-div1">
+<div className="cloth-border">
+
+</div>
+    </div>
+    <div className="cloth-card-div2">
+
+</div>
+    </div>
+    </div>
+
 
   </div>
 </div>
