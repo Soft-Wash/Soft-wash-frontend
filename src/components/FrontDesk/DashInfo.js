9283fee38d9b4e2bfff1344f701e62a6e5a2d2e9
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2024-01-19 00:03:14 +0100

    working on madal to assign task id to washman

--- src/components/FrontDesk/DashInfo.js
+++ src/components/FrontDesk/DashInfo.js
@@ -14,22 +14,62 @@ export default function DashInfo() {
   const [washmen, setWashmen] = useState([]);
   const [pendingPayments, setPendingPayments] = useState([]);
   const [pendingPaymentsCount, setPendingPaymentsCount] = useState(0);
-  const [allWashmen, setAllWashmen] = useState([
-    {task: 'free', _id: '655e49bad160aea8372bde1d', fullName: 'emmanuel fakaa', email: 'fakaashagbaor@gmail.com', password: '$2a$12$I0yZq2Cvyi/Wqp/DpfGUAuqXKopZycFFmJC9O7m5dtgoJqWM67XzC',  },
-    {task: 'free', _id: '655f441ab91ae3b48248ee3b', fullName: 'shegun dambola', email: 'fakaashagbaor2@gmail.com', password: '$2a$12$aSetUkxMFo5jqqD49VYH0u7t0lWDcktEUKsQhKf.MZLlAhnUeU2km',  },
-    {task: 'free', _id: '65606acde74306759caae67b', fullName: 'Gerald Fakaa', email: 'theboyfakaa@gmail.com', password: '$2a$12$WAr3XLeanz6oeSBkzD5oMeBSKZZmZOcRHHhZhxxZeth7JR1LI.Gjm',  },
-    {_id: '6569edd3abca2b034bf71180', fullName: 'Jerry Peter', email: 'jp@gmail.com', password: '$2a$12$lmlJqINTLg3dnsZ5hGP2XeDNrnHN.Kz0LL6L.n7JHuu.7gM2kQkoS', age: 28,  },
-    {_id: '656b07aa97952ceb72ca7a52', fullName: 'Embabs', email: 'emixwrld@gmail.com', password: '$2a$12$sq5VeaQhW94nLHgAmP0OLOc9gbVaee8Xq/03Wf6MW/dAYpvgpac1K', branch: '655debd5ec7b0b6e0f591bf9',  },
-    {_id: '659d575fb0c17d76a2d9b669', fullName: 'Joel Obi', email: 'joelobi@gmail.com', password: '$2a$12$2vKDwbCQUjLWIxk1iuLaIOY4E8fRtr3Gwj/H6h/XROpNnq2fd7rl.', branch: '655debc4ec7b0b6e0f591bf7',  }])
-    ;
+  // const [allWashmen, setAllWashmen] = useState([
+  //   {
+  //     task: "free",
+  //     _id: "655e49bad160aea8372bde1d",
+  //     fullName: "emmanuel fakaa",
+  //     email: "fakaashagbaor@gmail.com",
+  //     password: "$2a$12$I0yZq2Cvyi/Wqp/DpfGUAuqXKopZycFFmJC9O7m5dtgoJqWM67XzC",
+  //   },
+  //   {
+  //     task: "free",
+  //     _id: "655f441ab91ae3b48248ee3b",
+  //     fullName: "shegun dambola",
+  //     email: "fakaashagbaor2@gmail.com",
+  //     password: "$2a$12$aSetUkxMFo5jqqD49VYH0u7t0lWDcktEUKsQhKf.MZLlAhnUeU2km",
+  //   },
+  //   {
+  //     task: "free",
+  //     _id: "65606acde74306759caae67b",
+  //     fullName: "Gerald Fakaa",
+  //     email: "theboyfakaa@gmail.com",
+  //     password: "$2a$12$WAr3XLeanz6oeSBkzD5oMeBSKZZmZOcRHHhZhxxZeth7JR1LI.Gjm",
+  //   },
+  //   {
+  //     _id: "6569edd3abca2b034bf71180",
+  //     fullName: "Jerry Peter",
+  //     email: "jp@gmail.com",
+  //     password: "$2a$12$lmlJqINTLg3dnsZ5hGP2XeDNrnHN.Kz0LL6L.n7JHuu.7gM2kQkoS",
+  //     age: 28,
+  //   },
+  //   {
+  //     _id: "656b07aa97952ceb72ca7a52",
+  //     fullName: "Embabs",
+  //     email: "emixwrld@gmail.com",
+  //     password: "$2a$12$sq5VeaQhW94nLHgAmP0OLOc9gbVaee8Xq/03Wf6MW/dAYpvgpac1K",
+  //     branch: "655debd5ec7b0b6e0f591bf9",
+  //   },
+  //   {
+  //     _id: "659d575fb0c17d76a2d9b669",
+  //     fullName: "Joel Obi",
+  //     email: "joelobi@gmail.com",
+  //     password: "$2a$12$2vKDwbCQUjLWIxk1iuLaIOY4E8fRtr3Gwj/H6h/XROpNnq2fd7rl.",
+  //     branch: "655debc4ec7b0b6e0f591bf7",
+  //   },
+  // ]);
   const [loading, setLoading] = useState(true);
 
-
   const getOrders = async () => {
-    let resp = await axios.get(`${process.env.REACT_APP_BASE_URL}/order/day`);
+    try {
+      let resp = await axios.get(`${process.env.REACT_APP_BASE_URL}/order/day`);
     // console.log(resp)
     setDayOrderCount(resp.data.length);
     setDayOrders(resp.data);
+    setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
   };
 
   const getPendingPayments = async () => {
@@ -45,15 +85,15 @@ export default function DashInfo() {
       let resp = await axios.get(
         `${process.env.REACT_APP_BASE_URL}/employees/655e0053e7d633fca7e7f255/employee`
       );
-      // setWashmen(resp.data);
-      // console.log(resp.data)
+      setWashmen(resp.data);
+      console.log(resp.data)
       // this.setAllWashmen({
       //   allWashmen: resp.data
       // }, () => {
       //   console.log(this.state.allWashmen)
       // })
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
@@ -134,32 +174,33 @@ export default function DashInfo() {
               </small>
             </Col>
           </Row>
-          { loading ? 
-                      (<div className="w-100 h-75 d-flex justify-content-center">
-                      <div className="d-flex flex-column m-auto">
-                        <Spinner
-                          className="m-auto"
-                          animation="grow"
-                          variant="primary"
-                        />
-                        <p className=" m-auto">No orders yet!</p>
-                      </div>
-                    </div>) :
-            (dayOrders &&
+          {loading ? (
+            <div className="w-100 h-100 d-flex justify-content-center">
+              <div className="d-flex flex-column m-auto">
+                <Spinner
+                  className="m-auto"
+                  animation="grow"
+                  variant="primary"
+                />
+                <p className=" m-auto">No orders yet!</p>
+              </div>
+            </div>
+          ) : (
+            dayOrders &&
             dayOrders.map((order, index) => (
               <AnAppointment
                 key={index}
                 time={order.pickuptime}
                 name={order.customer_id}
                 status={order.status}
-              />)
+              />
             ))
-            }
+          )}
         </Col>
 
-        <Col className="border bg-white rounded-4" lg={5}></Col>
+        <Col className="border bg-white rounded-4 " lg={5}></Col>
 
-        <Col className="border bg-white rounded-4">
+        <Col className="border bg-white rounded-4 overflow-y-scroll">
           <Row className=" pt-2 pb-2 fw-bold justify-content-between">
             <Col
               lg={11}
@@ -171,21 +212,20 @@ export default function DashInfo() {
               <small className="text-success">Available Washmen</small>
               <small> (Open to Orders)</small>
             </Col>
-
-            {/* <WashmanProp image={img2} name={"Gerald Fakaa"} status={"free"}/>
-            <WashmanProp image={img2} name={"Dambola Segun"} status={'unfree'}/>
-            <WashmanProp image={img2} name={"Seyi Owolabi"} status={"free"}/> */}
-
-
           </Row>
-          <Row>
-                        {/* {allWashmen.length > 0 ? (
-              allWashmen &&
-              allWashmen.map((washman, index) => (
-                <WashmanProp name={washman.fullName} status={washman.task} />
+
+          {
+            washmen &&
+            washmen.length > 0 ? (
+              washmen.map((washman, index) => (
+                <WashmanProp
+                  name={washman.fullName}
+                  status={washman.task}
+                  key={index}
+                />
               ))
             ) : (
-              <div className="w-100 h-75 d-flex justify-content-center">
+              <div className="w-100 h-100 d-flex justify-content-center">
                 <div className="d-flex flex-column m-auto">
                   <Spinner
                     className="m-auto"
@@ -195,14 +235,8 @@ export default function DashInfo() {
                   <p className=" m-auto">Loading!</p>
                 </div>
               </div>
-            )} */}
-            {
-              // allWashmen &&
-              allWashmen.map((washman, index) => (
-                <WashmanProp name={washman.fullName} status={washman.task} key={index}/>
-              ))
-            }
-          </Row>
+            )
+          }
         </Col>
       </Row>
     </div>
--- src/components/FrontDesk/WashmanProp.js
+++ src/components/FrontDesk/WashmanProp.js
@@ -2,6 +2,11 @@ import { Col, Row } from "react-bootstrap";
 import img1 from "../../assets/washman-pic.jpg";
 
 export default function WashmanProp(props) {
+
+  const handleSubmit = () => {
+
+  }
+
   return (
     <>
       <div className="d-flex justify-content-between mb-4">
@@ -14,7 +19,7 @@ export default function WashmanProp(props) {
             style={{ width: "30px", height: "auto" }}
           />
         </div>
-        <div className="text-left  fw-semibold text-secondary text-start text-break px-0 lh-1"
+        <div className="text-left text-capitalize fw-semibold text-secondary text-start text-break px-2 me-1 lh-1"
         style={{ fontSize: "15px", width:"12rem" }}>
           <small className="text-lighter lh-1 my-auto">{props.name || "Dambola Segun (Stain spec.)"}</small>
         </div>
@@ -23,13 +28,23 @@ export default function WashmanProp(props) {
           className={` badge ${
             props.status == "free"
               ? "bg-success-subtle text-success"
-              : "bg-danger-subtle text-danger"
+              : "bg-warning-subtle text-warning"
           } my-1 `}
         >
-          <small className="" style={{ width: "20px", overflow: "hidden" }}>
+          <small className="my-auto" style={{ width: "20px", overflow: "hidden" }}>
             {props.status || "free"}
           </small>
         </Col>
+
+        <Row className="position-absolute bg-secondary opacity-25 top-0 start-0" style={{height:"100dvh", width:"100dvw"}}>
+          <div className="m-auto w-25 h-25 bg-white rounded ">
+            <h4 className="w-100 mx-auto ">Assign Task</h4>
+            <form onSubmit={(e) => handleSubmit(e.target)} className="h-75 d-flex flex-column justify-content-evenly">
+              <input type="text" placeholder="Task Id"/>
+              <button className="btn btn-md btn-outline-primary rounded">Assign</button>
+            </form>
+          </div>
+        </Row>
       </div>
     </>
   );
