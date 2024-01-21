fa5bafbbe546a98118005dd0a396e3b32fee6e19
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2024-01-21 23:30:15 +0100

    fixed testimony responsiveness

--- src/common/Testimonial.js
+++ src/common/Testimonial.js
@@ -8,21 +8,25 @@ export default function Testimonial(props) {
   return (
     <Card
       className=" mb-5 border-0"
-      style={{ height: "260px", width: "310px", marginLeft:"45px"}}
+      style={{ height: "280px", width: "300px", marginLeft:"30px"}}
     >
       <Card.Body className="text-center pb-0">
         <Row className="d-flex gap-1">
           <Col lg={4} md={4} sm={4} className="position-relative ">
             <img src={testimonialIcon} alt="icon" className="img-fluid position-relative p-0" style={{width:"100px", left:"-45px"}}/>
           </Col>
-          <Col lg={7} md={7} sm={7}  className="text-secondary  px-0" style={{}}>{props.review}</Col>
+          <Col lg={7} md={7} sm={7}  className="text-secondary  px-0 min-h-sm-40" 
+          // style={{minHeight: "90px"}}
+          >
+            {props.review}
+            </Col>
         </Row>
         <hr/>
-        <Row className="d-flex ">
-          <Col lg={5} md={5} sm={5}>
-            <img src={props.image || Client1} alt="client" className="rounded-circle " style={{height:"65px", width:"auto"}}/>
+        <Row >
+          <Col lg={5} md={5} sm="auto" xs="auto" >
+            <img src={props.image || Client1} alt="client" className="rounded-circle " style={{height:"65px", width:"65px"}}/>
           </Col>
-          <Col lg={7} md={6} sm={3}>
+          <Col lg={7} md={6} sm="auto" xs="auto" >
             <Row className={"fs-4 fw-semibold"}>{props.name}</Row>
             <Row>{"⭐".repeat(props.rating)}</Row>
           </Col>
