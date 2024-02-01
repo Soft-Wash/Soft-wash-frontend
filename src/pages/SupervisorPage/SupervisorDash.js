30e9ac79c5f3cc6c4f711fef308caae00acdb295
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2024-02-01 13:36:53 +0100

    Revert "Merge branch 'development' of https://github.com/Soft-Wash/Soft-wash-frontend into frontdesk"
    This reverts commit d4d4d9267c9533122bde2e832017d382e0169277, reversing
    changes made to 9110a7a1516ec2d3946a0d0ab5d10c84f700de97.

--- src/pages/SupervisorPage/SupervisorDash.js
+++ src/pages/SupervisorPage/SupervisorDash.js
@@ -1,9 +1,6 @@
 import AdminSidebar from "../../components/Admin/AdminSidebar";
 import "../../styles/SupervisorStyles/supervisordash.css";
 import { FaClipboardList, FaListAlt, FaRegUser} from "react-icons/fa";
-import { RiMoneyDollarCircleLine } from "react-icons/ri";
-import { MdOutlineAttachMoney } from "react-icons/md";
-import { LuAnchor } from "react-icons/lu";
 import { FaChalkboardUser } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import { BiCaretUp,BiCaretDown   } from "react-icons/bi";
@@ -22,12 +19,6 @@ function SupervisorDash() {
   const [branches, setbranches] = useState();
   const [orders, setOrders] = useState();
   const [Employees, setEmployees] = useState();
-  const [transactions, setTransactions] = useState();
-  const [totalAmount, setTotalamount] = useState();
-  const [expense, setExpense] = useState();
-  const [totalexpense, setTotalexpense] = useState();
-  const [totalrevenue, setTotalrevenue] = useState();
-
     const getTimeOfDay = () => {
         const currentHour = moment().hour();
     
@@ -61,81 +52,11 @@ function SupervisorDash() {
           console.error('Error fetching employees:', error);
         }
       };
-
-      const fetchTransactions = async () => {
-        try {
-          const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/transactions`);
-      
-          setTransactions(response.data.data.transactions)
-          // const transactions = response.data.data.transactions;
-          
-
-          // const totalAmount = transactions.reduce((sum, transaction) => sum + transaction.amount, 0);
-          // setTotalamount(totalAmount);
-
-          // console.log(totalAmount);
-
-      
-        } catch (error) {
-          // Handle errors (e.g., network issues, server errors)
-          console.error('Error fetching transactions:', error.message);
-        }
-      };
-
-      const fetchExpense = async () => {
-        try {
-          const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/expense`);
-      
-          // console.log(response.data)
-          setExpense(response.data)
-         
-          if (Array.isArray(response.data)) {
-            const totalExpense = response.data.reduce((sum, expense) => {
-              // Ensure amount is a number before adding to the sum
-              const expenseAmount = typeof expense.amount === 'number' ? expense.amount : 0;
-              return sum + expenseAmount;
-            }, 0);
-      
-            setTotalexpense(totalExpense);
-            console.log(totalExpense);
-          }
-
-      
-        } catch (error) {
-          // Handle errors (e.g., network issues, server errors)
-          console.error('Error fetching transactions:', error.message);
-        }
-      };
-  
   
       if (targetBranchId) {
         fetchEmployees();
-        fetchTransactions();
-        fetchExpense()
       }
     }, [targetBranchId]);
-
-    useEffect(() => {
-      // Calculate totalAmount whenever transactions state changes
-      if (transactions) {
-        // const updatedTotalAmount = transactions.reduce((sum, transaction) => sum + transaction.amount, 0);
-        // setTotalamount(updatedTotalAmount);
-        // console.log(updatedTotalAmount);
-        const totalAmount = transactions.reduce((sum, transaction) => sum + transaction.amount, 0);
-          setTotalamount(totalAmount);
-
-          console.log(totalAmount);
-      }
-    }, [transactions]);
-
-    useEffect(() =>{
-      if (transactions && expense){
-
-        const totalRevenue = totalAmount - totalexpense;
-
-        setTotalrevenue(totalRevenue)
-      }
-    })
 
     const getEmployeesId = (roleName) => {
       if (Employees) {
@@ -163,54 +84,41 @@ function SupervisorDash() {
                 <h4 className="">Good {timeOfDay}! Supervisor</h4>
                 <hr className="dashboard-line" />
     
-                <section1 className="HeaderCards-Container mb-3">
-                    <div className="Pair1">
-                      <Link to="/SupOrderTable"  className=" AllOrder sup-Card washman-grey"style={{textDecoration:"None"}}>
-                          <FaClipboardList className="supervisor-dashboard-icons "/>
-                          <div>
-                              <h5>All Orders</h5>
-                              <h5>{orders?.length}</h5>
-                          </div>
-                      </Link>
-                      <div className="AllUsers sup-Card washman-grey">
-                          <FaRegUser className="supervisor-dashboard-icons "/>
-                          <div>
-                              <h5>Employees</h5>
-                              <h5> {Employees?.length}</h5>
-                          </div>
-                      </div>
+                <section1 className="HeaderCards-Container mb-4">
+                    <div className=" AllOrder sup-Card washman-blue">
+                        <FaClipboardList className="supervisor-dashboard-icons "/>
+                        <div>
+                            <h5>All Orders</h5>
+                            <h5>{orders?.length}</h5>
+                        </div>
                     </div>
-                    <div className="Pair1">
-                      <div className="FrontDesk sup-Card washman-grey">
-                          <FaChalkboardUser className="supervisor-dashboard-icons "/>
-                          <div>
-                              <h5>FrontDesk</h5>
-                              <h5> {FrontDesk?.length || 0}</h5>
-                          </div>
-                      </div>
-                      <div className="Washman sup-Card washman-grey">
-                          <FaChalkboardUser className="supervisor-dashboard-icons "/>
-                          <div>
-                              <h5>Washman</h5>
-                              <h5>{washman?.length || 0}</h5>
-                          </div>
-                      </div>
+                    <div className="AllUsers sup-Card washman-purple">
+                        <FaRegUser className="supervisor-dashboard-icons "/>
+                        <div>
+                            <h5>All Users</h5>
+                            <h5> {Employees?.length}</h5>
+                        </div>
                     </div>
-                    <div className="Pair1">
-                      <div className="Inventory sup-Card washman-green">
-                          <FaChalkboardUser className="supervisor-dashboard-icons "/>
-                          <div>
-                              <h5>Transactions</h5>
-                              <h5>{transactions?.length}</h5>
-                          </div>
-                      </div>
-                      <div className="Inventory sup-Card washman-green">
-                          <FaChalkboardUser className="supervisor-dashboard-icons "/>
-                          <div>
-                              <h5>Inventory</h5>
-                              <h5>3</h5>
-                          </div>
-                      </div>
+                    <div className="FrontDesk sup-Card washman-green">
+                        <FaChalkboardUser className="supervisor-dashboard-icons "/>
+                        <div>
+                            <h5>FrontDesk</h5>
+                            <h5> {FrontDesk?.length || 0}</h5>
+                        </div>
+                    </div>
+                    <div className="Washman sup-Card washman-blue">
+                        <FaChalkboardUser className="supervisor-dashboard-icons "/>
+                        <div>
+                            <h5>Washman</h5>
+                            <h5>{washman?.length || 0}</h5>
+                        </div>
+                    </div>
+                    <div className="Inventory sup-Card washman-green">
+                        <FaChalkboardUser className="supervisor-dashboard-icons "/>
+                        <div>
+                            <h5>Inventory</h5>
+                            <h5>3</h5>
+                        </div>
                     </div>
                     {/* <div className="icon-container mb-3">
                         <div className="icon-container-innerd1">
@@ -271,45 +179,7 @@ function SupervisorDash() {
                         </div>
                     </div>   */}
                 </section1>
-                <section2 className="HeaderCards-Container mb-4">
-                    <div className=" AllOrder sup-Card2 washman-purple">
-                        <MdOutlineAttachMoney className="supervisor-dashboard-icons-Big "/>
-                        <div>
-                            <h5>Total Sales</h5>
-                            <h5>{totalAmount}</h5>
-                        </div>
-                    </div>
-                    <div className="AllUsers sup-Card2 washman-green">
-                        <LuAnchor className="supervisor-dashboard-icons-Big "/>
-                        <div>
-                            <h5>Total Expense</h5>
-                            <h5> {totalexpense}</h5>
-                        </div>
-                    </div>
-                    <div className="FrontDesk sup-Card2 washman-blue">
-                        <RiMoneyDollarCircleLine className="supervisor-dashboard-icons-Big "/>
-                        
-                        <div>
-                            <h5>Total Revenue</h5>
-                            <h5> {totalrevenue}</h5>
-                        </div>
-                    </div>
-                    {/* <div className="Washman sup-Card washman-blue">
-                        <FaChalkboardUser className="supervisor-dashboard-icons "/>
-                        <div>
-                            <h5>Washman</h5>
-                            <h5>{washman?.length || 0}</h5>
-                        </div>
-                    </div> */}
-                    {/* <div className="Inventory sup-Card washman-green">
-                        <FaChalkboardUser className="supervisor-dashboard-icons "/>
-                        <div>
-                            <h5>Inventory</h5>
-                            <h5>3</h5>
-                        </div>
-                    </div> */}
-                </section2>
-                <section3 className="visuals d-flex ">
+                <section2 className="visuals d-flex ">
                     <div className="Left" style={{width:"90%", gap:"10px"}}>
                         {/* <InventoryChart/> */}
                         <SupDashChart/>
@@ -319,7 +189,7 @@ function SupervisorDash() {
                         {/* <SupervisorCarousel/> */}
                         <SupervisorTeam/>
                     </div>
-                </section3>
+                </section2>
                 
             </div>
         </div>
