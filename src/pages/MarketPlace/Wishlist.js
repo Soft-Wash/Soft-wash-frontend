d1a3d70ee27d4af6b7f89efb339840e158a6314a
Gbadass <theboyfakaa@gmail.com>
2024-02-11 20:33:44 +0100

    updated wishlist

--- src/pages/MarketPlace/Wishlist.js
+++ src/pages/MarketPlace/Wishlist.js
@@ -3,17 +3,15 @@ import { Container, Col, Row, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../../styles/Wishlist.css";
 import { FaRegShareSquare } from "react-icons/fa";
-import itemImg1 from "../../assets/MarketPlace/Images/1507033810106_sptows2927_so_klin_detergent_190_g_1024x1024.jpg";
-import itemImg2 from "../../assets/MarketPlace/Images/1499349122495_spxspz1244_waw_hyper-concentrated_multi-use_detergent_200_g_180x2x.jpg";
-import itemImg3 from "../../assets/MarketPlace/Images/1509621985884_sptows2785_ariel_original_perfume_detergent_powder_1_kg_1024x1024.jpg";
-import itemImg4 from "../../assets/MarketPlace/Images/1516383180440_spxty4380_waw_multi-purpose_soap_250_g_180x2x2.jpg";
-import itemImg5 from "../../assets/MarketPlace/Images/Afer-Ironing-Board-Maxi-130-x-47-cm-Supermart-ng-9833_180x2x.jpg";
-import itemImg6 from "../../assets/MarketPlace/Images/1465908830684_spxspy1512_nittol_anti-bacterial_multi-purpose_soap_150_g_180x2x2.jpg";
 import { FiHeart } from "react-icons/fi";
 import Button from "react-bootstrap/Button";
 import Footer from "../../common/Footer";
 import axios from "axios";
 import { useEffect,useState } from "react";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { axiosInstance } from "../../services/AxiosInstance";
 
 function Wishlist() {
   const [wishlist,setWishlist] = useState()
@@ -28,12 +26,40 @@ function Wishlist() {
     })
   }
 
+  const addToCart = (item_id) => {
+    const CustomerData = JSON.parse(localStorage.getItem("softwashLoginUser"));
+    const Customer_id = CustomerData._id;
+    const cartData = {
+      product_id: item_id,
+      quantity: 1,
+      customer_id: Customer_id,
+    };
+    axiosInstance
+      .post("/cart/create", cartData)
+      .then((resp) => {
+        console.log(resp.data);
+        toast.success("item added to cart");
+      })
+      .catch((error) => {
+        console.error("Error adding item to cart:", error);
+        if (error.response) {
+          const status = error.response.status;
+          if (status === 400) {
+            toast.error("item already in cart");
+          } else {
+            toast.error("Unexpected error occurred. Please try again later.");
+          }
+        }
+      });
+  };
+
   useEffect(()=>{
     getWishList()
   },[])
 
   return (
     <div>
+            <ToastContainer position="top-center" />
       <Navigation />
       <Container className="text-center text-secondary pt-5">
         <h1>Wishlist</h1>
@@ -71,6 +97,7 @@ function Wishlist() {
                       <Button
                         variant="secondary"
                         className="cart-button bg-info border-0 w-75 rounded-0"
+                        onClick={()=>addToCart(item.product._id)}
                       >
                         Add to Cart
                       </Button>{" "}
