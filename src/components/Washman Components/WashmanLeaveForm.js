dcba681582958957c1382aad442a26ece0ef8d3d
Nate <nathbivan@gmail.com>
2024-02-21 00:50:46 +0100

    loader

--- src/components/Washman Components/WashmanLeaveForm.js
+++ src/components/Washman Components/WashmanLeaveForm.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import axios from "axios";
+import Loader from "../Loader/Loader";
 
 
 function WashmanLeaveForm() {
@@ -19,7 +20,9 @@ function WashmanLeaveForm() {
 
   const [washman, setWashman] = useState({});
   const [branchID, setBranchID] = useState("")
-    const [error, setError] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
 
 
 
@@ -40,6 +43,7 @@ function WashmanLeaveForm() {
 
         const fetchWashman = async () =>{          
             try{
+                setLoading(true)
                 const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/employees/${washmanID}`)
                 setWashman(response.data);
                 console.log(response.data);    
@@ -110,98 +114,99 @@ function WashmanLeaveForm() {
 
   return (
     <div className="washman-bg">
+      {loading ? <Loader /> : 
       <div className="washman-page-content">
-        <div className="washman-header">
-          <h2>LEAVE APPLICATION</h2>
-        </div>
-        <div>
-          <Form className="centralize  mt-4" onSubmit={handleSubmit}>
-            <Form.Group className="washman-edit-profile-input">
-              <Form.Label htmlFor="fullName" className="reset-input-headers">
-                Full Name
-              </Form.Label>
-              <Form.Control
-                type="text"
-                placeholder={washman.fullName}
-                value={washman.fullName}
-                onChange={(e) => setFullName(e.target.value)}
-                />
-              {err && fullName === "" ? (
-                <span className="reset-err-msg">Kindly enter your Full Name</span>
-              ) : null}
-              
-
-              <Form.Label htmlFor="email" className="reset-input-headers">
-                Email address
-              </Form.Label>
-              <Form.Control
-                type="email"
-                placeholder={washman.email}
-                value={washman.email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              {err && email === "" ? (
-                <span className="reset-err-msg">Kindly enter your email</span>
-              ) : null}
-
-              <Form.Label htmlFor="reasons" className="reset-input-headers">
-                Reasons
-              </Form.Label>
-              <Form.Control
-                type="text"
-                placeholder=""
-                value={reasons}
-                onChange={(e) => setReasons(e.target.value)}
-              />
-              {err && reasons === "" ? (
-                <span className="reset-err-msg">Kindly enter your Reason For Leave</span>
-              ) : null}
-
-              <select  className="reset-input-headers" onChange={(e) => setSelectedOption(e.target.value)}>
-                <option value="" hidden>
-                  Select Status
-                </option>
-                <option value="annual">ANNUAL</option>
-                <option value="burial">BURIAL</option>
-                <option value="sick">SICK</option>   
-                <option value="wedding">WEDDING</option>              
-                <option value="patarnity">PATARNITY</option>              
-                <option value="matarnity">MATARNITY</option>                          
-            </select>
-                
-              <Form.Label htmlFor="startDate" className="reset-input-headers">
-                Start Date
-              </Form.Label>
-              <Form.Control
-                type="date"
-                placeholder=""
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)}
-              />
-              {err && startDate === "" ? (
-                <span className="reset-err-msg">Kindly enter the date you wish to start your Leave</span>
-              ) : null}
-
-              <Form.Label htmlFor="endDate" className="reset-input-headers">
-                End Date
-              </Form.Label>
-              <Form.Control
-                type="date"
-                placeholder=""
-                value={endDate}
-                onChange={(e) => setEndDate(e.target.value)}
-              />
-              {err && endDate === "" ? (
-                <span className="reset-err-msg">Kindly enter the date you wish to end your Leave</span>
-              ) : null}
-            </Form.Group>
-
-            <Button className="edit-washman-profile-btn" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </div>
+      <div className="washman-header">
+        <h2>LEAVE APPLICATION</h2>
       </div>
+      <div>
+        <Form className="centralize  mt-4" onSubmit={handleSubmit}>
+          <Form.Group className="washman-edit-profile-input">
+            <Form.Label htmlFor="fullName" className="reset-input-headers">
+              Full Name
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder={washman.fullName}
+              value={washman.fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              />
+            {err && fullName === "" ? (
+              <span className="reset-err-msg">Kindly enter your Full Name</span>
+            ) : null}
+            
+
+            <Form.Label htmlFor="email" className="reset-input-headers">
+              Email address
+            </Form.Label>
+            <Form.Control
+              type="email"
+              placeholder={washman.email}
+              value={washman.email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {err && email === "" ? (
+              <span className="reset-err-msg">Kindly enter your email</span>
+            ) : null}
+
+            <Form.Label htmlFor="reasons" className="reset-input-headers">
+              Reasons
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder=""
+              value={reasons}
+              onChange={(e) => setReasons(e.target.value)}
+            />
+            {err && reasons === "" ? (
+              <span className="reset-err-msg">Kindly enter your Reason For Leave</span>
+            ) : null}
+
+            <select  className="reset-input-headers" onChange={(e) => setSelectedOption(e.target.value)}>
+              <option value="" hidden>
+                Select Status
+              </option>
+              <option value="annual">ANNUAL</option>
+              <option value="burial">BURIAL</option>
+              <option value="sick">SICK</option>   
+              <option value="wedding">WEDDING</option>              
+              <option value="patarnity">PATARNITY</option>              
+              <option value="matarnity">MATARNITY</option>                          
+          </select>
+              
+            <Form.Label htmlFor="startDate" className="reset-input-headers">
+              Start Date
+            </Form.Label>
+            <Form.Control
+              type="date"
+              placeholder=""
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+            {err && startDate === "" ? (
+              <span className="reset-err-msg">Kindly enter the date you wish to start your Leave</span>
+            ) : null}
+
+            <Form.Label htmlFor="endDate" className="reset-input-headers">
+              End Date
+            </Form.Label>
+            <Form.Control
+              type="date"
+              placeholder=""
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+            {err && endDate === "" ? (
+              <span className="reset-err-msg">Kindly enter the date you wish to end your Leave</span>
+            ) : null}
+          </Form.Group>
+
+          <Button className="edit-washman-profile-btn" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>}
 
       {/* Modal for pending leave application */}
       {/* <div  */}
