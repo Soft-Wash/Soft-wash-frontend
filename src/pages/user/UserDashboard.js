84d2e56f2707a9c4ff24962f6b99b5db6b3b9120
Gbadass <theboyfakaa@gmail.com>
2024-02-21 23:47:01 +0100

    updated review

--- src/pages/user/UserDashboard.js
+++ src/pages/user/UserDashboard.js
@@ -1,49 +1,56 @@
 import Sidebar from "../../components/OrdersPage/Sidebar";
 import { FaClipboardList, FaListAlt } from "react-icons/fa";
 import "../../styles/UserDashboard.css";
-import '../../styles/DashboardContact.css';
+import "../../styles/DashboardContact.css";
 import { useEffect } from "react";
 import { axiosInstance } from "../../services/AxiosInstance";
 import { useState } from "react";
 import axios from "axios";
 import UserSidebarTablet from "../../components/UserSidebarTablet";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function UserDashboard() {
   const [orders, setOrders] = useState();
-  const [paymentLength,setpaymentLength]=useState()
+  const [paymentLength, setpaymentLength] = useState();
 
-
-
-  const GetUserOrders=()=>{
+  const GetUserOrders = () => {
     const userId = JSON.parse(localStorage.getItem("softwashLoginUser"));
-    axios.get(`${process.env.REACT_APP_BASE_URL}/order/${userId._id}/allorders`).then((resp) => {
-      setOrders(resp.data);
-    });
-  }
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/order/${userId._id}/allorders`)
+      .then((resp) => {
+        setOrders(resp.data);
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
 
-  const getPayment=()=>{
+  const getPayment = () => {
     const userId = JSON.parse(localStorage.getItem("softwashLoginUser"));
 
     axios
-      .get(
-        `${process.env.REACT_APP_BASE_URL}/payments/all/userpayments`,
-        {
-          params: { id: userId._id},
-        }
-      )
+      .get(`${process.env.REACT_APP_BASE_URL}/payments/all/userpayments`, {
+        params: { id: userId._id },
+      })
       .then((resp) => {
         console.log(resp.data);
         setpaymentLength(resp.data);
-      });
-  }
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });;
+  };
 
   useEffect(() => {
-    getPayment()
-    GetUserOrders()
+    getPayment();
+    GetUserOrders();
   }, []);
 
   return (
     <div>
+      <ToastContainer position="top-center" />
       <div>
         <UserSidebarTablet />
       </div>
